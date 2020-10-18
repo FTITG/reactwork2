e2c8faadcb0d87c30a6f7f068d43f808dfbe0eee
zhaoyue <2241633500@qq.com>
2020-10-18 20:05:33 +0800

    add

---
+++ precache-manifest.96c1d30909bd16e54edcb9183582f9c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "642c046febc4e6fef23b4b6d6f121587",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31eb054f85917b2336ac",
+    "url": "/static/css/main.a3a8b971.chunk.css"
+  },
+  {
+    "revision": "39883644ae20acc0dfd7",
+    "url": "/static/js/2.62472819.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/static/js/2.62472819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31eb054f85917b2336ac",
+    "url": "/static/js/main.091c48ee.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
